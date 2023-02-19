c3197a8fc834c4649a47f21b73de3f46a5ece4f0
salem676 <echoesofeternal@gmail.com>
2023-02-19 12:44:55 -0600

    vstido

---
+++ 0x06-signals/10-pid_exist.c
@@ -0,0 +1,14 @@
+#include <errno.h>
+#include <signal.h>
+
+int pid_exist(pid_t pid);
+
+/**
+ * pid_exist - checks if a given pid represents an existing process
+ * @pid: process id to check
+ * Return: 1 if process exists | 0 if not
+ **/
+int pid_exist(pid_t pid)
+{
+	return (kill(pid, 0) < 1 && errno != ESRCH);
+}
