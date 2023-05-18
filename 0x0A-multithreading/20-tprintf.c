3ef554f27f57e70ce44cb421b397ab530a44b15e
salem676 <echoesofeternal@gmail.com>
2023-05-18 09:56:48 -0600

    pastor budista

--- 0x0A-multithreading/20-tprintf.c
+++ 0x0A-multithreading/20-tprintf.c
@@ -1,43 +1,49 @@
 #include "multithreading.h"
 
-static pthread_mutex_t mutex;
+static pthread_mutex_t lock_mutex;
+
+void initialize_mutex(void) __attribute__((constructor));
+
+void destroy_mutex(void) __attribute__((destructor));
 
 /**
- * mutex_destroy - destroy mutex
- */
-__attribute__((destructor))void mutex_destroy(void)
+* initialize_mutex - initialize the mutex
+* Return: Nothing
+*/
+void initialize_mutex(void)
 {
-	if (pthread_mutex_destroy(&mutex))
-		perror(NULL);
+	pthread_mutex_init(&lock_mutex, NULL);
 }
 
 /**
- * mutex_init - initialise mutex
- */
-__attribute__((constructor))void mutex_init(void)
+* destroy_mutex - destroy the mutex
+* Return: Nothing
+*/
+void destroy_mutex(void)
 {
-	if (pthread_mutex_init(&mutex, NULL) != 0)
-		perror(NULL);
+	pthread_mutex_destroy(&lock_mutex);
 }
 
 /**
- * tprintf - print out given formatted string
- * @format: string to print
- *
- * Return: number of characters printed
+ * tprintf -  print out a given formatted string and the  calling thread ID
+ * @format: is string format
+ * Return: 0 on success
  */
 int tprintf(char const *format, ...)
 {
-	va_list ap;
-	int c;
+	va_list args;
+	pthread_t self;
 
-	if (pthread_mutex_lock(&mutex) != 0)
-		perror(NULL);
-	va_start(ap, format);
-	c = printf("[%lu] ", pthread_self());
-	c += vprintf(format, ap);
-	va_end(ap);
-	if (pthread_mutex_unlock(&mutex) != 0)
-		perror(NULL);
-	return (c);
+	pthread_mutex_lock(&lock_mutex);
+
+	self = pthread_self();
+	setbuf(stdout, NULL);
+	va_start(args, format);
+	printf("[%lu] ", self);
+	vprintf(format, args);
+	va_end(args);
+
+	pthread_mutex_unlock(&lock_mutex);
+
+	return (0);
 }
--- 0x0A-multithreading/20-tprintf.c~
+++ 0x0A-multithreading/20-tprintf.c~
@@ -1,32 +1,43 @@
+#include "multithreading.h"
+
+static pthread_mutex_t mutex;
+
 /**
- * tprintf - uses printf family to print out a given formatted string
- *			 uses mutex to prevent race conditions
- * @format: formatted string
+ * mutex_destroy - destroy mutex
+ */
+__attribute__((destructor))void mutex_destroy(void)
+{
+	if (pthread_mutex_destroy(&mutex))
+		perror(NULL);
+}
+
+/**
+ * mutex_init - initialise mutex
+ */
+__attribute__((constructor))void mutex_init(void)
+{
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+		perror(NULL);
+}
+
+/**
+ * tprintf - print out given formatted string
+ * @format: string to print
+ *
  * Return: number of characters printed
- **/
+ */
 int tprintf(char const *format, ...)
 {
-	pthread_t self = pthread_self();
-	va_list args;
-	int chars_printed;
+	va_list ap;
+	int c;
 
-	va_start(args, format);
-	pthread_mutex_lock(&tprintf_mutex);
-	chars_printed = printf("[%lu] ", (unsigned long)self);
-	chars_printed += vprintf(format, args);
-	pthread_mutex_unlock(&tprintf_mutex);
-	va_end(args);
-	return (chars_printed);
+	if (pthread_mutex_lock(&mutex) != 0)
+		perror(NULL);
+	va_start(ap, format);
+	c = printf("[%lu] ", pthread_self());
+	c += vprintf(format, ap);
+	va_end(ap);
+	if (pthread_mutex_unlock(&mutex) != 0)
+		perror(NULL);
+	return (c);
 }
-
-__attribute__((constructor)) void tprintf_mutex_init(void)
-{
-	pthread_mutex_init(&tprintf_mutex, NULL);
-}
-
-__attribute__((destructor)) void tprintf_mutex_destroy(void)
-{
-	pthread_mutex_destroy(&tprintf_mutex);
-}
-
-void end(void) __attribute__((destructor));
