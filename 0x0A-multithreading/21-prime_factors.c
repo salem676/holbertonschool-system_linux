e327eef34c8c3f14f8d95293221cd5cdb3d9b07e
salem676 <echoesofeternal@gmail.com>
2023-05-18 09:39:04 -0600

    cat

--- 0x0A-multithreading/21-prime_factors.c
+++ 0x0A-multithreading/21-prime_factors.c
@@ -1,21 +1,36 @@
-#include "list.h"
-#include <stdio.h>	/* debug */
+#include "multithreading.h"
+#include <stdlib.h>
 
 /**
- * prime_factors - factorise a number into a list of prime factors
- * @s: string representation of the number to factorise
- *
- * Return: list of prime factors
- */
+ * prime_factors - factors a number into a list of prime factors
+ * @s: string representationum of the number to factor
+ * Return: list_t of prime factors
+ **/
 list_t *prime_factors(char const *s)
 {
-	list_t *list;
+	unsigned long num = strtoul(s, NULL, 10);
+	unsigned long *temp, point = 2;
+	list_t *list = malloc(sizeof(list_t));
 
-	if (s == NULL)
-		return (NULL);
-	list = malloc(sizeof(*list));
-	if (list == NULL)
-		return (NULL);
-	list = list_init(list);
+	list_init(list);
+	while (point * point <= num)
+	{
+		while (num % point == 0)
+		{
+			temp = malloc(sizeof(unsigned long));
+			*temp = point;
+			list_add(list, (void *)temp);
+			num /= point;
+		}
+
+		point += 1 + (point != 2);
+	}
+
+	if (num >= 2)
+	{
+		temp = malloc(sizeof(unsigned long));
+		*temp = num;
+		list_add(list, (void *)temp);
+	}
 	return (list);
 }
